4b5bbb181f54fdba141002eeb553a3594c108b0d
jeliwag <jeliwag@MID-JELIWAG>
2026-01-28 12:10:19 +0800

    [fix] Logout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 
 public class AccountController : Controller
 {
-    [HttpGet]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult Logout()
     {
         return SignOut(
