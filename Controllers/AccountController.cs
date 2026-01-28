e8af2f7c652abc9a8a09d708268de08c152c7cfb
jeliwag <jeliwag@MID-JELIWAG>
2026-01-28 11:58:41 +0800

    [add] Logut

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,38 +1,11 @@
-﻿using Duende.IdentityServer.Services;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
-using SSO.Auth.Api.Models;
 
 public class AccountController : Controller
 {
-    private readonly IIdentityServerInteractionService _interaction;
-
-    public AccountController(IIdentityServerInteractionService interaction)
-    {
-        _interaction = interaction;
-    }
-
-    public IActionResult Login()
-    {
-        return Challenge(new AuthenticationProperties
-        {
-            RedirectUri = "/Dashboard/Index"
-        }, "oidc");
-    }
-
     [HttpGet]
-    public async Task<IActionResult> Logout(string logoutId)
-    {
-        var logoutContext = await _interaction.GetLogoutContextAsync(logoutId);
-
-        return View(new LogoutViewModel
-        {
-            LogoutId = logoutId,
-            PostLogoutRedirectUri = logoutContext?.PostLogoutRedirectUri
-        });
-    }
-
     public IActionResult Logout()
     {
         return SignOut(
@@ -40,20 +13,8 @@ public class AccountController : Controller
             {
                 RedirectUri = "/"
             },
-            CookieAuthenticationDefaults.AuthenticationScheme,
+            "vams_cookie",
             "oidc"
         );
     }
-
-    [HttpPost]
-    public async Task<IActionResult> LogoutConfirmed(string logoutId)
-    {
-        await HttpContext.SignOutAsync();
-
-        var logoutContext = await _interaction.GetLogoutContextAsync(logoutId);
-
-        return Redirect(logoutContext?.PostLogoutRedirectUri ?? "/");
-    }
-
-
 }
